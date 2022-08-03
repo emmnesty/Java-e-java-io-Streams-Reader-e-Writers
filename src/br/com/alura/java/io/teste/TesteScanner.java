67628c6cd653813daa2b87341cdbf0532c608153
emmnesty <elyjhonatan@hotmail.com>
2022-08-03 16:49:42 -0300

    Classe Scanner

---
+++ src/br/com/alura/java/io/teste/TesteScanner.java
@@ -0,0 +1,21 @@
+package br.com.alura.java.io.teste;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TesteScanner {
+
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(new File("contas.csv"));
+
+		while (sc.hasNextLine()) {
+			String linha = sc.nextLine();
+			System.out.println(linha);
+
+		}
+
+		sc.close();
+
+	}
+
+}
