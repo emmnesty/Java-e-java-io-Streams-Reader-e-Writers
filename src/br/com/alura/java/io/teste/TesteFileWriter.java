89a26a73a9433451a7ad4f36f46f17d6afe07ea2
emmnesty <elyjhonatan@hotmail.com>
2022-08-03 15:52:59 -0300

    FileWriter

---
+++ src/br/com/alura/java/io/teste/TesteFileWriter.java
@@ -0,0 +1,29 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteFileWriter {
+
+	public static void main(String[] args) throws IOException {
+
+//		OutputStream fos = new FileOutputStream("lorem2.txt");
+//		Writer osw = new OutputStreamWriter(fos);
+//		BufferedWriter bw = new BufferedWriter(osw);
+		
+		BufferedWriter bw = new BufferedWriter(new FileWriter("lorem3.txt"));
+
+		bw.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod");
+		bw.newLine();
+		bw.newLine();
+		bw.write("teste blabla bla ");
+
+		bw.close();
+
+	}
+}
