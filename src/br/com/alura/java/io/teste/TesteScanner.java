34afffccc0c94547830a7aa395f68999d42a8123
emmnesty <elyjhonatan@hotmail.com>
2022-08-03 17:14:36 -0300

    parsing file

--- src/br/com/alura/java/io/teste/TesteScanner.java
+++ src/br/com/alura/java/io/teste/TesteScanner.java
@@ -1,17 +1,34 @@
 package br.com.alura.java.io.teste;
 
 import java.io.File;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TesteScanner {
 
 	public static void main(String[] args) throws Exception {
+
 		Scanner sc = new Scanner(new File("contas.csv"));
 
 		while (sc.hasNextLine()) {
 			String linha = sc.nextLine();
 			System.out.println(linha);
 
+			Scanner linhaScanner = new Scanner(linha);
+			linhaScanner.useLocale(Locale.US);
+			linhaScanner.useDelimiter(",");
+
+			String valor1 = linhaScanner.next();
+			int valor2 = linhaScanner.nextInt();
+			int valor3 = linhaScanner.nextInt();
+			String valor4 = linhaScanner.next();
+			double valor5 = linhaScanner.nextDouble();
+
+			System.out.println(valor1 + " " + valor2 + " " + valor3 + " " + valor4 + " " + valor5);
+
+			linhaScanner.close();
+//			String[] valores = linha.split(",");
+//			System.out.println(valores[3]);
 		}
 
 		sc.close();
